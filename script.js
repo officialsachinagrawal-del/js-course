f7f7d998690b13a44fab2830bac809648a6ceaa3
officialsachinagrawal-del <officialsachinagrawal@gmail.com>
2025-09-18 00:10:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+//for loop
+let sum = 0;
+// let n= 12;
+for(var i =0 ;i<=12 ;i++){
+    sum += i;
+}
+console.log(i)
+console.log(sum)
+
+
+
+//for in loop
+
+/*const obj = {
+    harry:55,
+    subh:56,
+    sachin:50,
+    nandu:"i love you"
+}
+// for(a in obj){
+//     console.log(obj[a]);
+// }
+let arr = [45,4,4,6];
+for(a in arr){
+    console.log(arr[a]);
+}
+*/
+// for of loop
+/*
+for(a of "harray"){
+    console.log(a)
+}
+*/
